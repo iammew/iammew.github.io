a70027b2c8c0fad2a2f562cde1b03f063b1df62a
iammew <iam@mew.wang>
2024-05-20 17:48:11 +0800

    update

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ $(document).ready(function() {
     if(user == 'dewu'){
         $('.container').children().hide();
         $('.container').children(':first').show();
-        $('#js-countdown').html('我以为今年是获得幸福的一年。很遗憾。两个人的想法不能靠近。<br>那确实我不该那样。<br>回看了以往的记录，或许我和前边你见过的男生都是一样的，你尝试相处了，可是发现不合适了。只是我以为我是不一样的，有点高估了自己。<br>我原来什么都不是。<br>这个分站页面就到这里吧。<br><br>2024.5.20<br>本页面无限期下线中');
+        $('#js-countdown').html('<a style="font-size:1.5em">本页面无限期下线中</a><br><br>我以为今年是获得幸福的一年。很遗憾。两个人的想法不能靠近。<br>所以我如果再做什么就真的不礼貌了（虽然说这句话很好笑）。<br>回看了以往的记录，或许我和前边你见过的男生都是一样的，你尝试相处了，可是发现不合适了。只是我以为我是不一样的，有点高估了自己。<br><a style="font-size:1.2em;color:orange">原来我什么都不是。</a><br>这个分站页面就运行到这里吧。<br>2024.5.20');
         $('#typeMenu').hide();
     }
     $('.main-version__down').on('click', function(){
